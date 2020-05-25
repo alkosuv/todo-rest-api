658f62b205f13cf931c4e5c1d7894480bd802b2c
gem95mis <al.ko.suv@gmail.com>
2020-05-25 11:31:59 +0300

    upd middleware

--- internal/api/v1/routers/userrouter.go
+++ internal/api/v1/routers/userrouter.go
@@ -4,7 +4,18 @@ import (
 	"encoding/json"
 	"net/http"
 
+<<<<<<< 61c8efc38422bf61a314b8c1c1be0141007a8ca5
 	"github.com/gen95mis/todo-rest-api/internal/api/v1/model"
+=======
+<<<<<<< 5a94ea24bd541a184c7b9038b9b1e7bb19c70e7e
+=======
+<<<<<<< HEAD
+	"github.com/gen95mis/todo-rest-api/internal/api/v1/middleware"
+=======
+>>>>>>> parent of 5a94ea2... upd middleware
+	"github.com/gen95mis/todo-rest-api/internal/api/v1/model"
+>>>>>>> upd middleware
+>>>>>>> upd middleware
 	"github.com/gen95mis/todo-rest-api/internal/api/v1/response"
 	"github.com/gen95mis/todo-rest-api/internal/api/v1/store"
 	"github.com/gorilla/mux"
